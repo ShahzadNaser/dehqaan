817c8e86aba52080490f276d29514d679bd733d7
root <erpnextaddon@gmail.com>
2022-08-09 18:43:06 +0000

    purchase order report

---
+++ dehqaan/dehqaan/report/purchase_order_details/purchase_order_details.js
@@ -0,0 +1,58 @@
+// Copyright (c) 2016, Blox and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Purchase Order Details"] = {
+	"filters": [
+		{
+                        "fieldname":"company",
+                        "label": __("Company"),
+                        "fieldtype": "Link",
+                        "options": "Company",
+                        "default": frappe.defaults.get_user_default("Company")
+                },
+		{
+			"fieldname":"from_date",
+			"label": __("From Range"),
+			"fieldtype": "Date",
+			"reqd": 1
+		},
+		{
+                        "fieldname":"to_date",
+                        "label": __("To Range"),
+                        "fieldtype": "Date",
+                        "reqd": 1
+                },
+		{	
+			"fieldname":"workflow_state",
+			"label": __("Workflow State"),
+			"fieldtype": "Link",
+			"options": "Workflow State"
+		},
+		{
+		        "fieldname":"supplier",
+                        "label": __("Supplier"),
+                        "fieldtype": "Link",
+                        "options": "Supplier"
+                },
+		{
+                        "fieldname":"project",
+                        "label": __("Project"),
+                        "fieldtype": "Link",
+                        "options": "Project"
+                },
+		{
+                        "fieldname":"department",
+                        "label": __("Department"),
+                        "fieldtype": "Link",
+                        "options": "Cost Center"
+                }
+
+	],
+	"tree": true,
+	"parent_field": "parent_department",
+	"name_field": "department",
+	"initial_depth": 2
+
+}
+
---
+++ dehqaan/dehqaan/report/purchase_order_details/purchase_order_details.py
@@ -0,0 +1,174 @@
+# Copyright (c) 2013, Dehaqan and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import flt
+from frappe import _
+
+
+def execute(filters=None):
+    columns, data = [], []
+    departments  = get_all_departments(filters)
+    columns = get_columns()
+    order_details = get_data(filters)
+
+    totals = frappe._dict({})
+    totals.update({
+                "indent": 0.0, "name":'Grand Total', "parent_department":'',
+                        "department": '', "workflow_state": '', "project": '',
+                        "project_name":'', "supplier_name": '', "grand_total":0.0,
+                        "outstanding": 0.0,'transaction_date':'','paid':0.0
+                })
+    
+    blank_totals = frappe._dict({})
+    blank_totals.update({
+                "indent": 0.0, "name":'', "parent_department":'',
+                        "department": '', "workflow_state": '', "project": '',
+                        "project_name":'', "supplier_name": '', "grand_total":'',
+                        "outstanding": 0.0,'transaction_date':'','paid':0.0
+
+                })
+
+    
+    for department in departments:
+        data.append({
+			"indent": 0.0, "is_parent": True,
+			"parent_department": "",
+			"department":department.department,
+		})
+
+        department_totals = frappe._dict({})
+        department_totals.update({
+                "indent": 0.0, "name":'Total', "parent_department":'',
+                        "department": '', "workflow_state": '', "project": '',
+                        "project_name":'', "supplier_name": '', "grand_total":0.0,
+                        "outstanding": 0.0,'transaction_date':'','paid':0.0
+
+                })
+
+        for ss in order_details:
+            
+            if department.department == ss.cost_center:
+                temp = frappe._dict({})
+                temp.update({
+			"indent": 1.0, "name": ss.name, "parent_department": ss.cost_center,
+			"department": '', "workflow_state": ss.workflow_state, "project": ss.project,
+			"project_name": ss.project_name, "supplier_name": ss.supplier_name, "grand_total": ss.grand_total,
+                        "outstanding": 0,'transaction_date':ss.transaction_date,'paid':0.0
+                })
+
+                payment_details = frappe.db.sql("""select purchase_order,sum(outstanding_amount) as outstanding
+                                            from `tabPurchase Invoice Item` pii
+                                            inner join `tabPurchase Invoice` pi
+                                            on pii.parent=pi.name
+                                            where purchase_order = '%s'
+                                            group by purchase_order"""%ss.name,as_dict=1)
+                payment_amount = 0
+                if payment_details:
+                    for payment in payment_details:
+                        payment_amount = ss.grand_total - payment.outstanding
+
+                temp.paid = payment_amount
+                if temp.paid == 0:
+                    if ss.advance_paid:
+                        temp.paid = ss.advance_paid
+
+                data.append(temp)
+                totals.grand_total += ss.grand_total
+                totals.outstanding += ss.outstanding
+                totals.paid += temp.paid
+
+                department_totals.paid += temp.paid
+                department_totals.grand_total += ss.grand_total
+                department_totals.outstanding += ss.outstanding
+        #data.append(blank_totals)
+        data.append(department_totals)
+    
+    data.append(totals)
+    return columns, data
+
+def get_columns():
+    columns = [{
+			"fieldname": "department",
+			"label": "Department",
+			"width": 250,
+			"fieldtype": "Link",
+                        "options": "Cost Center"
+		},{
+			"fieldname": "name",
+			"label": "Purchase Order",
+			"width": 150,
+			"fieldtype": "Link",
+			"options": "Purchase Order"
+		},
+                {
+                        "fieldname": "transaction_date",
+                        "label": "Date",
+                        "width": 100,
+                        "fieldtype": "Date"
+                },
+                {
+                        "fieldname":"workflow_state",
+			"label": "Workflow State",
+			"width": 150,
+			"fieldtype": "Data"
+		},
+                {
+                        "fieldname":"project",
+                        "label": "Project",
+                        "width": 150,
+                        "fieldtype": "Link",
+                        "options": "Project"
+                },
+                {
+                        "fieldname":"project_name",
+                        "label": "Project Name",
+                        "width": 150,
+                        "fieldtype": "Data"
+                },
+                {
+                        "fieldname":"supplier_name",
+                        "label": "Supplier Name",
+                        "width": 150,
+                        "fieldtype": "Data"
+                },
+                {
+                        "fieldname":"grand_total",
+                        "label": "Grand Total",
+                        "width": 110,
+                        "fieldtype": "Currency"
+                        
+                },
+                {
+                        "fieldname":"paid",
+                        "label": "Paid Amount",
+                        "width": 120,
+                        "fieldtype": "Currency"
+                }
+
+	]
+    return columns
+
+def get_all_departments(filters):
+    departments = frappe.db.sql("""select distinct name as department from `tabCost Center` where company = '%s' and is_group = 0""" %filters.company, as_dict=1)
+    return departments
+
+def get_data(filters):
+    cond = " and company = '%s' and transaction_date between '%s' and '%s'"%(filters.company,filters.from_date,filters.to_date)
+
+    if filters.workflow_state:
+        cond += " and workflow_state = '%s'"%filters.workflow_state
+
+    if filters.supplier:
+        cond += " and supplier = '%s'"%filters.supplier
+
+    if filters.project:
+        cond += " and project = '%s'"%filters.project
+
+    if filters.department:
+        cond += " and cost_center = '%s'"%filters.department
+
+
+
+    return frappe.db.sql("""select name,advance_paid,transaction_date,cost_center,project_name,supplier_name,workflow_state,project,supplier,grand_total,0 as outstanding from `tabPurchase Order` where docstatus=1 %s """%cond,as_dict=1)
